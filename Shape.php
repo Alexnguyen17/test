951e574fa4518a06c2ad1bb92c982ad4f85f6d03
Alexnguyen17 <Alexnguyen170395@gmail.com>
2018-10-31 13:27:55 +0700

    test1

---
+++ Shape.php
@@ -0,0 +1,26 @@
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexnguyen
+ * Date: 31/10/2018
+ * Time: 09:49
+ */
+
+namespace ape;
+
+
+class Shape{
+    public $name;
+
+    public function show()
+    {
+        
+    }
+        class Rectangle extends Shape
+        {
+        public $width;
+        public $height;
+    }
+
+}
